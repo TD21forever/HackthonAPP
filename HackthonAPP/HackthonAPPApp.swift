7495c73bac8fd84cb15b2cde6b320a07ad748ea5
TD21forever <435212619@qq.com>
2022-04-16 08:57:24 +0800

    Initial Commit

---
+++ HackthonAPP/HackthonAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackthonAPPApp.swift
+//  HackthonAPP
+//
+//  Created by T D on 2022/4/16.
+//
+
+import SwiftUI
+
+@main
+struct HackthonAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
